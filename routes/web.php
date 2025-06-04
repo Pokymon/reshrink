bf9243ab4210952838619b890c9f17a647c9d684
Fauqi Amar Hizbullah <a@fauqi.com>
2025-06-04 16:27:52 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,13 @@ Route::get('/', function () {
   return Inertia::render('welcome');
 })->name('home');
 
-Route::get('/@{link}', [LinkController::class, 'show'])->name('links.show');
+Route::get('/{link:short_url}', [LinkController::class, 'show'])->name('links.show');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-  Route::get('/dashboard', [LinkController::class, 'index'])->name('dashboard');
-  Route::post('/links', [LinkController::class, 'store'])->name('links.store');
-  Route::put('/links/{link:id}', [LinkController::class, 'update'])->name('links.update');
-  Route::delete('/links/{link:id}', [LinkController::class, 'destroy'])->name('links.destroy');
+  Route::get('/my/dashboard', [LinkController::class, 'index'])->name('dashboard');
+  Route::post('/my/links', [LinkController::class, 'store'])->name('links.store');
+  Route::patch('/my/links/{link:id}', [LinkController::class, 'update'])->name('links.update');
+  Route::delete('/my/links/{link:id}', [LinkController::class, 'destroy'])->name('links.destroy');
 });
 
 require __DIR__ . '/settings.php';
